bb5c6f5b14d9322cd8b482675b0254a4d4dfead9
Iuldash <yldash1davletov@gmail.com>
2026-07-31 23:02:28 +0200

    feat: connect edaman api

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import AppRouter from './router/AppRouter';
 
-
-
 function App() {
   return (
     <>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,48 @@
-import Header from "../../components/header/Header";
-import RecipeCard from './RecipeCard'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from '../../components/header/Header';
+import RecipeCard from './RecipeCard';
 
 export default function Home() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const BASE_URL = 'https://api.edamam.com/api/recipes/v2';
+
+    const fetchRecipes = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(BASE_URL,
+          {
+            params: {
+              type: 'public',
+              q: 'chicken',
+              app_id: import.meta.env.VITE_APP_ID,
+              app_key: import.meta.env.VITE_APP_KEY,
+            }
+          }
+        );
+        setRecipes(res.data.hits);
+      }
+      catch (error) {
+        console.error(error)
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+    fetchRecipes();
+  }, [])
+
   return (
     <div>
-    <Header/>
-    <RecipeCard/>
+      <Header />
+      <RecipeCard />
     </div>
-  )
+  );
+
+
 }
